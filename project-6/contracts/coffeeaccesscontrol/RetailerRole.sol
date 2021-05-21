8487216462009495f2acbbd53b4546dd643ca764
bijzz <user@anonynmous.com>
2021-05-21 21:11:31 +0200

    Extend accesscontrol roles

--- project-6/contracts/coffeeaccesscontrol/RetailerRole.sol
+++ project-6/contracts/coffeeaccesscontrol/RetailerRole.sol
@@ -5,14 +5,18 @@ import "./Roles.sol";
 
 // Define a contract 'RetailerRole' to manage this role - add, remove, check
 contract RetailerRole {
+  using Roles for Roles.Role;
 
   // Define 2 events, one for Adding, and other for Removing
-  
+  event RetailerAdded(address indexed account);
+  event RetailerRemoved(address indexed account);
+
   // Define a struct 'retailers' by inheriting from 'Roles' library, struct Role
+  Roles.Role private reailers;
 
   // In the constructor make the address that deploys this contract the 1st retailer
   constructor() public {
-    
+    _addRetailer(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
